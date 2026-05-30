76f7c82229b1088aab602411a01548515f4a451b
Cristian Díaz <cristian@210x7.io>
2026-05-30 20:53:59 +0200

    Publish 0.3.126 macOS arm64 SwiftUsdShell binaries

--- Package.swift
+++ Package.swift
@@ -18,18 +18,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.126-macos-arm64.2"),
+        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.126-macos-arm64.3"),
     ],
     targets: [
         .binaryTarget(
             name: "SwiftUsdShell",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/0.3.126-macos-arm64.2/Artifacts/v0.3.126-macos-arm64.2/SwiftUsdShell-0.3.126-macos-arm64.2.xcframework.zip",
-            checksum: "d4110c0a1b076be091b6c0550512bb71aadb1805ffba7ea5f2969368680f8977"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/0.3.126-macos-arm64.3/Artifacts/v0.3.126-macos-arm64.3/SwiftUsdShell-0.3.126-macos-arm64.3.xcframework.zip",
+            checksum: "51eb3e362348b1f2b231b93ecc8a99faa88363243527dfe8779f454cf0576c45"
         ),
         .binaryTarget(
             name: "SwiftUsdShellOpenUSD",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/0.3.126-macos-arm64.2/Artifacts/v0.3.126-macos-arm64.2/SwiftUsdShellOpenUSD-0.3.126-macos-arm64.2.xcframework.zip",
-            checksum: "1925859bd409d6b903329552a8fc41b1e7b9997a18f0a3e76bd5a058b78c268e"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/0.3.126-macos-arm64.3/Artifacts/v0.3.126-macos-arm64.3/SwiftUsdShellOpenUSD-0.3.126-macos-arm64.3.xcframework.zip",
+            checksum: "d9b01e55e400e02dcbf49854092568073b16756be6dd47eff927f419bbdba131"
         ),
         .target(
             name: "_SwiftUsdShellOpenUSDLink",
