08b8bcb3e142b79ffd3ac8848e4dbdfb482ac0bf
Cristian Díaz <cristian@210x7.io>
2026-06-03 11:19:53 +0200

    Publish SwiftUsdShell iOS arm64 binary train 0.3.128

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUsdShell",
     platforms: [
-        .macOS(.v15),
+        .iOS(.v26),
     ],
     products: [
         .library(name: "SwiftUsdShell", targets: ["SwiftUsdShell"]),
@@ -18,18 +18,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.127-macos-arm64.2"),
+        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.128-ios-arm64.1"),
     ],
     targets: [
         .binaryTarget(
             name: "SwiftUsdShell",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.127-macos-arm64.2/Artifacts/v0.3.127-macos-arm64.2/SwiftUsdShell-0.3.127-macos-arm64.2.xcframework.zip",
-            checksum: "b81f5ec8d30ae2337555bde1cf9ca1496122dd81592ba48f40f19721f5652910"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.128-ios-arm64.1/Artifacts/v0.3.128-ios-arm64.1/SwiftUsdShell-0.3.128-ios-arm64.1.xcframework.zip",
+            checksum: "0a826a9e5995dcb6ee3fcc01b64d34fe1076464582afbb596d2e5319daded879"
         ),
         .binaryTarget(
             name: "SwiftUsdShellOpenUSD",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.127-macos-arm64.2/Artifacts/v0.3.127-macos-arm64.2/SwiftUsdShellOpenUSD-0.3.127-macos-arm64.2.xcframework.zip",
-            checksum: "e1f6c55e95b526d08cf2ef54fe1c3244f5597c1ac366cf45aaec9d8df9cda8e2"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.128-ios-arm64.1/Artifacts/v0.3.128-ios-arm64.1/SwiftUsdShellOpenUSD-0.3.128-ios-arm64.1.xcframework.zip",
+            checksum: "a32ada9e6b4ab9c1f652a19e654afcab3ab2e90a6921b97d6258782fb7557235"
         ),
         .target(
             name: "_SwiftUsdShellOpenUSDLink",
