d1998a84f3fccfbebc06e7dc9cfc4899d833b06d
Cristian Díaz <cristian@210x7.io>
2026-06-04 21:18:51 +0200

    Release 0.3.133-ios-arm64.1 iOS shell binaries

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version: 6.2
 import PackageDescription
 
-// Auto-generated binary release manifest for iOS and macOS Apple Silicon.
 let package = Package(
     name: "SwiftUsdShell",
     platforms: [
         .iOS(.v26),
-        .macOS(.v14),
     ],
     products: [
         .library(name: "SwiftUsdShell", targets: ["SwiftUsdShell"]),
@@ -20,23 +18,22 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.131-apple-arm64.4"),
+        .package(url: "https://github.com/Reality2713/SwiftUsd-binaries.git", exact: "0.3.133-ios-arm64.1"),
     ],
     targets: [
         .binaryTarget(
             name: "SwiftUsdShell",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.132-apple-arm64.1/Artifacts/v0.3.132-apple-arm64.1/SwiftUsdShell-0.3.132-apple-arm64.1.xcframework.zip",
-            checksum: "ddfa3cd3724a0d4f2514dfe41c511f8e112495f998eed108f63d00609c746fbd"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.133-ios-arm64.1/Artifacts/v0.3.133-ios-arm64.1/SwiftUsdShell-0.3.133-ios-arm64.1.xcframework.zip",
+            checksum: "a1d0b753b309b5ae623bbef0e388b9fb373f2c75baed9677043940d2ba6726a4"
         ),
         .binaryTarget(
             name: "SwiftUsdShellOpenUSD",
-            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.132-apple-arm64.1/Artifacts/v0.3.132-apple-arm64.1/SwiftUsdShellOpenUSD-0.3.132-apple-arm64.1.xcframework.zip",
-            checksum: "e75f3e0e208cb4b6d1b44632c7f884b0e5e468de3a38a3ad28ef43256c2a5328"
+            url: "https://raw.githubusercontent.com/Reality2713/SwiftUsdShell-binaries/v0.3.133-ios-arm64.1/Artifacts/v0.3.133-ios-arm64.1/SwiftUsdShellOpenUSD-0.3.133-ios-arm64.1.xcframework.zip",
+            checksum: "4b3f6ac10c89309171e5770e1a2c6a25ab5c5cbf34a155fe94cc63a4ad57aa78"
         ),
         .target(
             name: "_SwiftUsdShellOpenUSDLink",
             dependencies: [
-                .product(name: "_OpenUSD_SwiftBindingHelpers", package: "SwiftUsd-binaries"),
                 .product(name: "OpenUSD", package: "SwiftUsd-binaries"),
             ],
             swiftSettings: [.interoperabilityMode(.Cxx)]
